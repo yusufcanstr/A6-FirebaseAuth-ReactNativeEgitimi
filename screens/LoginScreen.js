32c3e65f36a8f83a91aafbeff3b66816ac1cc190
Yusufcan Senturk <developer.yusufcan@gmail.com>
2024-04-30 18:19:59 +0300

    UseState İle Email ve Şifre Bilgisinin Tutulması

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -6,14 +6,28 @@ import {
   TextInput,
   TouchableOpacity,
 } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 
 export default function LoginScreen() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <KeyboardAvoidingView style={styles.container} behavior="padding">
       <View style={styles.inputContainer}>
-        <TextInput style={styles.input} placeholder="Email" />
-        <TextInput style={styles.input} placeholder="Password" />
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          secureTextEntry
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
       </View>
       <View style={styles.buttonContainer}>
         <TouchableOpacity style={styles.button}>
@@ -28,46 +42,43 @@ export default function LoginScreen() {
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    inputContainer:{
-        width: '80%',
-    },
-    input:{
-        backgroundColor: 'white',
-        paddingHorizontal: 15,
-        paddingVertical: 10,
-        marginTop: 10,
-        borderRadius: 10,
-    },
-    buttonContainer:{
-        width:'60%',
-        marginTop: 40,
-
-    },
-    button:{
-        backgroundColor: '#2596be',
-        padding: 12,
-        alignItems: 'center',
-        borderRadius: 10,
-
-    },
-    buttonText:{
-        color: 'white',
-        fontSize: 16,
-        fontWeight: 'bold'
-    },
-    outlineButton:{
-        backgroundColor: 'white',
-        marginTop: 7,
-
-    },
-    outlineButtonText:{
-        color: '#2596be',
-        fontSize: 16,
-        fontWeight: 'bold'
-    }
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  inputContainer: {
+    width: "80%",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    marginTop: 10,
+    borderRadius: 10,
+  },
+  buttonContainer: {
+    width: "60%",
+    marginTop: 40,
+  },
+  button: {
+    backgroundColor: "#2596be",
+    padding: 12,
+    alignItems: "center",
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  outlineButton: {
+    backgroundColor: "white",
+    marginTop: 7,
+  },
+  outlineButtonText: {
+    color: "#2596be",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
 });
